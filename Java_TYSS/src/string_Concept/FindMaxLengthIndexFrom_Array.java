ac2672ce8c8b7644a20eb37dcbe656f2f3173e99
karthikmshetty <karthikmshetty108@gmail.com>
2023-06-08 14:24:21 +0530

    s

---
+++ Java_TYSS/src/string_Concept/FindMaxLengthIndexFrom_Array.java
@@ -0,0 +1,28 @@
+package string_Concept;
+
+public class FindMaxLengthIndexFrom_Array {
+
+	public static void main(String[] args) {
+String [] s= {"ab","bcde","hbcv","ec"};
+		
+		String min_len=s[0];
+		for(int i=0;i<s.length;i++)
+		{
+			if(s[i].length()>min_len.length())
+			{
+				min_len=s[i];
+			}
+		}
+		
+		for(int i=0;i<s.length;i++)
+		{
+			if(s[i].length()==min_len.length())
+			{
+				System.out.println(s[i]+" "+i);
+			}
+		}
+		
+		
+	}
+
+}
---
+++ Java_TYSS/src/string_Concept/FindSumOf2Digits.java
@@ -0,0 +1,27 @@
+package string_Concept;
+
+public class FindSumOf2Digits {
+
+	public static void main(String[] args) {
+
+		
+		String s="a11b12d13"; //11+12+13
+		int sum=0;
+		int tsum=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)>='0'&&s.charAt(i)<='9')
+			{
+				int n=s.charAt(i)-48; 
+				tsum=tsum*10+n;
+			}
+			else
+			{
+				sum=sum+tsum;
+				tsum=0;
+			}
+	}
+		System.out.println(sum);
+
+
+}}
