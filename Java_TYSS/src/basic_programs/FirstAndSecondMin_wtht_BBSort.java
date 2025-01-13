7704b017529999a4b4dd6ed2ff4596665f76b239
karthikmshetty <135118312+karthikmshetty@users.noreply.github.com>
2025-01-13 15:51:42 +0530

    Update FirstAndSecondMin_wtht_BBSort.java

--- Java_TYSS/src/basic_programs/FirstAndSecondMin_wtht_BBSort.java
+++ Java_TYSS/src/basic_programs/FirstAndSecondMin_wtht_BBSort.java
@@ -1,55 +1,26 @@
 package basic_programs;
 
 public class FirstAndSecondMin_wtht_BBSort {
-
 	public static void main(String[] args) {
-int []a= {0,3,4,0};
 
-	int min1=a[0];
-	int min2=a[1];
-	
-	for(int i=0;i<a.length;i++)
-	{
-		if(a[i]<min1)
-		{
-			min1=a[i];
+		int[] a = {0, 3, 4, 0};
+
+		// Initialize the first and second minimums to the maximum value
+		int min1 = Integer.MAX_VALUE;
+		int min2 = Integer.MAX_VALUE;
+
+		for (int num : a) {
+			if (num < min1) {
+				// Update both min1 and min2
+				min2 = min1;
+				min1 = num;
+			} else if (num < min2 && num != min1) {
+				// Update only min2 if it's not equal to min1
+				min2 = num;
+			}
 		}
-		
-	}
-	
-	for(int i=0;i<a.length;i++)
-	{
-		if(a[i]<min2&&a[i]!=min1)
-		{
-			min2=a[i];
-		}
-		
-	}
-	
-	System.out.println("First two min nmbr-->"+min1+" "+min2);
-	
-	// for first and second max number
-	int max1=a[0];
-	int max2=a[0];
-	
-for(int i=0;i<a.length;i++)
-{
-if(a[i]>max1)
-	max1=a[i];
-}
 
-	
-	for(int i=0;i<a.length;i++)
-	{
-	if(a[i]>max2&&a[i]!=max1)	
-		max2=a[i];
+		System.out.println("First Minimum: " + min1);
+		System.out.println("Second Minimum: " + min2);
 	}
-	
-	System.out.println("First two max nmbr-->"+max1+" "+max2);
-
-	
-	}
-	
-
-
 }
